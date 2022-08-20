04d91244a82f77069e9922ad0af0796a0667f1c3
Desipeli <93584995+Desipeli@users.noreply.github.com>
2022-08-20 19:05:45 +0300

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog }) => {
+
+  const [showFullInfo, setShowFullInfo] = useState(false)
+
+  const clickHandler = () => {
+    setShowFullInfo(!showFullInfo)
+  }
+
+  const likeHandler = () => {
+    
+  }
+
+  return(
+    <div className={'blog'} >
+      <div className={'blog-first-row'} onClick={clickHandler}>
+        {blog.title} {blog.author}
+      </div>
+      {showFullInfo &&
+        <>
+        <br></br>
+          {blog.url}<br></br>
+          {blog.likes} <button onClick={likeHandler}>like</button><br></br>
+          {blog.user.username}<br></br>
+        </>
+      }
+    </div>  
+  )
+}
 
 export default Blog
